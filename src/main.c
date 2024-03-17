193db0f2f57501e16a2f386279bb230546ab67a0
Quartz563 <quartz135@gmail.com>
2024-03-17 21:31:57 +1300

    Create core logic of the game method

--- src/main.c
+++ src/main.c
@@ -2,10 +2,62 @@
 // Created by quart on 17/03/2024.
 //
 
-int main(){
-    return 0;
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int score = 0;
+char selection;
+
+
+int game(char user, char comp)
+{
+    if(user == comp)
+    {
+        return -1;
+    }
+    if(user == 'r')
+    {
+        if(comp == 'p')
+        {
+            return 0;
+        }
+        if( comp == 's')
+        {
+            return 1;
+        }
+
+    }
+    if(user == 'p')
+    {
+        if(comp == 's')
+        {
+            return 0;
+        }
+        if(comp == 'r')
+        {
+            return 1;
+        }
+    }
+    if(user == 's')
+    {
+        if(comp == 'r')
+        {
+            return 0;
+        }
+        if(comp == 'p')
+        {
+            return 1;
+        }
+    }
+    return -1;
 }
 
+int main()
+{
+
+}
 
 
 
